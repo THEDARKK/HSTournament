e6dad13c8b247829c901128aeaaec4e7038184be
Nimor111 <georgi.bojinov@hotmail.com>
2017-05-03 00:54:10 +0300

    get started on auth component

--- angular/src/app/components/auth.component.ts
+++ angular/src/app/components/auth.component.ts
@@ -1,2 +1,11 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../services/auth.service';
 
+
+@Component {
+    selector: 'my-auth',
+    providers: [AuthService],
+    templateUrl: 
+
+}
